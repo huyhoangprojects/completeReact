9e5742e5cc213e922b9950759bc460cfb8650fa4
huy-hoang.phan <huyhoangprojects@gmail.com>
2018-03-14 21:45:16 +0700

    Exploring JS

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,39 @@
 'use strict';
 
 var template = React.createElement(
-  'h1',
+  'div',
   null,
-  'Hello, world!'
+  React.createElement(
+    'h1',
+    null,
+    'Hello, world!'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'I\'m Phan Huy Ho\xE0ng'
+  )
+);
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Name: Huy Hoang Phan'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: 26'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: Binh Dinh'
+  )
 );
 var appRoot = document.getElementById('root');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
-const template = <h1>Hello, world!</h1>;
+const template = (
+  <div>
+    <h1>Hello, world!</h1>
+    <p>I'm Phan Huy Hoàng</p>
+  </div>
+);
+
+const templateTwo = (
+  <div>
+    <h1>Name: Huy Hoang Phan</h1>
+    <p>Age: 26</p>
+    <p>Location: Binh Dinh</p>
+  </div>
+);
 const appRoot = document.getElementById('root');
 
-ReactDOM.render(
-  template,
-  appRoot
-);
+ReactDOM.render(templateTwo, appRoot);
