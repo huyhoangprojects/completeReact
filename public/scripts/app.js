4c909fd5d05b97b6d8fad55f8365837315b09ec1
huy-hoang.phan <huyhoangprojects@gmail.com>
2018-03-14 21:03:30 +0700

    done setup

---
+++ public/scripts/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var template = React.createElement(
+  'h1',
+  null,
+  'Hello, world!'
+);
+var appRoot = document.getElementById('root');
+
+ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,7 @@
+const template = <h1>Hello, world!</h1>;
+const appRoot = document.getElementById('root');
+
+ReactDOM.render(
+  template,
+  appRoot
+);
