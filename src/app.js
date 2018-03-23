87af67998fecfdf9aec3ffcbf880de2d599e2490
huy-hoang.phan <huy-hoang.phan@ekino.com>
2018-03-23 02:52:29 +0000

    remove react cdn in index.html

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const template = <p>text v2</p>;
+const template = <p>text v3</p>;
 
 ReactDOM.render(template, document.getElementById('root'));
